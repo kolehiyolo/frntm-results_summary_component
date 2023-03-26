5252784016f6eed67a579b61f39bdfeea8862fe0
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-03-26 23:37:40 +0800

    cool

--- index.js
+++ index.js
@@ -14,58 +14,111 @@ let values = {
     }
 }
 
-function randomize() {
-    console.log(`randomize()`);
+function startCalculating() {
+    console.log(`startCalculating()`);
 
+    // * First, we produce random values for the scores
+    randomizeValues();
+
+    // * Then, we do a loop updating from the HTML from the top bar to the bottom
+    updateScoreBars();
+}
+
+function randomizeValues() {
+    console.log(`randomizeValues()`);
+
+    // * Assign random values between 0 and 100 for the user's pretend scores
     values.reaction = Math.floor(Math.random() * 101);
     values.memory = Math.floor(Math.random() * 101);
     values.verbal = Math.floor(Math.random() * 101);
     values.visual = Math.floor(Math.random() * 101);
 
-    values.score = Math.floor((values.reaction+values.memory+values.verbal+values.visual)/4);
+    let average = 100;
 
-    updateHTML();
-    // updateCardUpperColors();
-    countUpScore();
+    values.reaction = average;
+    values.memory = average;
+    values.verbal = average;
+    values.visual = average;
+
+    // * The final score will then be the average of the preceding 4 values
+    values.score = Math.floor((values.reaction + values.memory + values.verbal + values.visual) / 4);
 }
 
-function updateHTML() {
-    console.log(`updateHTML()`);
-    $(`.summary .reaction .score .result`).html(values.reaction);
-    $(`.summary .memory .score .result`).html(values.memory);
-    $(`.summary .verbal .score .result`).html(values.verbal);
-    $(`.summary .visual .score .result`).html(values.visual);
-    
-}
+function updateScoreBars() {
+    console.log(`updateScoreBars()`);
 
-function countUpScore( ) {
-    let currentScore = 0;
-    const counter = setInterval(function () {
-        currentScore++;
-        $(`.upper .score_bubble .score`).html(currentScore);
-        updateCardUpperColors(currentScore);
-        if (currentScore===values.score) {
-            clearInterval(counter);
-            $(`.upper .score_bubble .ripple`).removeClass(`ripple-default`);
-            $(`.upper .score_bubble .ripple`).addClass(`ripple-expanded`);
-            setInterval(function () {
-                $(`.upper .score_bubble .ripple`).removeClass(`ripple-expanded`);
-                $(`.upper .score_bubble .ripple`).addClass(`ripple-default`);
-            }, 1000);
+    // * Here we assign the order of how we'll present the scores
+    let categories = ["reaction", "memory", "verbal", "visual"];
+
+    // * Next, we'll do the loop chain of timers to do the count up for each category
+    let counterSpeed = 15;
+    let delaySpeed = 200;
+    let currentCategoryIndex = -1;
+    let currentCategory = categories[currentCategoryIndex];
+    let currentCategoryDone = true;
+    let currentCategoryScore = 0;
+    const parentCounter = setInterval(() => {
+        if (currentCategoryDone === true) {
+            currentCategoryIndex++;
+            currentCategory = categories[currentCategoryIndex];
+            currentCategoryDone = false;
+            currentCategoryScore = 0;
+            setTimeout(() => {
+                const counter = setInterval(function () {
+                    currentCategoryScore++;
+                    $(`.summary .${currentCategory} .score .result`).html(currentCategoryScore);
+                    $(`.${currentCategory} .fluid`).attr('style', `width: ${currentCategoryScore}%`);
+                    if (currentCategoryScore === values[currentCategory]) {
+                        currentCategoryDone = true;
+                        clearInterval(counter);
+                    }
+                }, counterSpeed);
+            }, delaySpeed);
         }
-    }, 25);
+        if (currentCategoryIndex === categories.length) {
+            // * Once that's done, we then update the gradiented card's content
+            setTimeout(() => {
+                updateVerdictCard();
+            }, delaySpeed);
+            clearInterval(parentCounter);
+        }
+    }, counterSpeed);
 }
 
-function updateCardUpperColors(currentScore) {
-    console.log(`updateCardUpperColors()`);
-    
-    // RED = 0
-    // BLUE = 200
-    let topHue = currentScore * 2;
-    let topBottom = topHue - 70;
+function updateVerdictCard() {
+    console.log(`updateVerdictCard()`);
 
-    values.color.top = `hsl(${topHue}, 100%, 67%)`;
-    values.color.bottom = `hsl(${topBottom}, 81%, 54%))`;
+    function countUpScore() {
+        let currentScore = 0;
+        const counter = setInterval(function () {
+            currentScore++;
+            $(`.upper .score_bubble .score`).html(currentScore);
+            updateCardUpperColors(currentScore);
+            if (currentScore === values.score) {
+                clearInterval(counter);
+                $(`.upper .score_bubble .ripple`).removeClass(`ripple-default`);
+                $(`.upper .score_bubble .ripple`).addClass(`ripple-expanded`);
+                setInterval(function () {
+                    $(`.upper .score_bubble .ripple`).removeClass(`ripple-expanded`);
+                    $(`.upper .score_bubble .ripple`).addClass(`ripple-default`);
+                }, 1000);
+            }
+        }, 25);
+    }
 
-    $(`.card .upper`).attr('style',`background-image: linear-gradient(${values.color.top}, ${values.color.bottom}`);
+    function updateCardUpperColors(currentScore) {
+        console.log(`updateCardUpperColors()`);
+
+        // RED = 0
+        // BLUE = 220
+        let topHue = currentScore * 2.3;
+        let topBottom = topHue - 20;
+
+        values.color.top = `hsl(${topHue}, 100%, 67%)`;
+        values.color.bottom = `hsl(${topBottom}, 81%, 54%))`;
+
+        $(`.card .upper`).attr('style', `background-image: linear-gradient(${values.color.top}, ${values.color.bottom}`);
+    }
+
+    countUpScore();
 }
---
+++ notes.md
@@ -0,0 +1,11 @@
+STARTING STATE:
+1. Left side card will only say say "Calculating Results..."
+2. Right side card will have all values to zero
+
+CALCULATING STATE:
+1. When the user clicks on the continue button, the reaction score and bar fills
+2. Once that's done, we proceed to do the same to memory, and the rest
+3. Once all that's done, the left-side colored card starts counting up, score-wise, and the gradient also adjusts accordingly
+4. Once the score is presented, the text below confirms the verdict
+
+FINAL STATE:
